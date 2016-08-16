6f8063170f997f18d04f4abec6bb318b531bd012
vaibhav3301 <vaibhavshelke017@gmail.com>
2016-08-16 15:59:34 +0530

    hmmm

--- Scripts/application/downloader.py
+++ Scripts/application/downloader.py
@@ -1,26 +1,21 @@
 import requests as rq
-from contextlib import closing
 
 class download(object):
 
 	link = ""
 	headers = ""
-	download_path_windows = "C:" 
 	
 	def __init__(self, link):
 		self.link = link
 
 	def get_headers(self):
-		response = rq.head(self.link)
+		response = rq.head(link)
 		self.headers = response.headers
 		return response.headers
 
-	def start(self):
-		"progress bar, and run in thread"
-		
-		with closing(rq.get(self.link, stream=True)) as r:
-			print r.headers['content-length']
-
+	def start_download(self):
+		l = link.split('/')[-1]
+    	print l
 
 	def write_file(self, f_name):
 		pass
--- Scripts/application/test.py
+++ Scripts/application/test.py
@@ -1,8 +1,19 @@
 import downloader as d
 
 
-first = d.download("http://randomusik.tk/songs/9.mp3")
+l = "https://www.youtube.com/watch?v=963TE8mh8VA"
+l2 = "http://randomusik.tk/songs/4.mp3"
+first = d.download(l2)
 
 print first.get_headers()['content-length']
 
-first.start()
+first.start_download()
+
+# import requests
+
+# r = requests.get("http://randomusik.tk/songs/4.mp3")
+
+# with open('test.mp3', 'wb') as f:
+# 	for chunk in r.iter_content(chunk_size=1024): 
+# 		if chunk:
+# 			f.write(chunk)
